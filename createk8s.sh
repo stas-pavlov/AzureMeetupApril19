21f0ac4b35d2df2db512a6bc17c8a54d87456133
Stas Pavlov <stanisp@microsoft.com>
2017-09-15 14:59:38 +0300

    typo fix

--- createk8s.sh
+++ createk8s.sh
@@ -19,14 +19,14 @@ az group create --name "${AZURE_RG_NAME}" --location "${AZURE_DC_LOCATION}"
 az acs create --resource-group="${AZURE_RG_NAME}" \
   --location="${AZURE_DC_LOCATION}" \
   --orchestrator-type=kubernetes \
-  --master-count=1 --agent-count=2 \
+  --master-count=2 --agent-count=3 \
   --agent-vm-size="Standard_D2_v2" \
   --admin-username="stask8sadmin" \
   --generate-ssh-keys \
   --name="${AZURE_SERVICE_NAME}" \
   --dns-prefix="${AZURE_DNS_PREFIX}"
 
-  read  -n 1 -p "Press any key to continue..."
+read  -n 1 -p "Press any key to continue..."
 
 # Downoad .kube configuration from created Kubernetes cluster to work with kubectl 
 az acs kubernetes get-credentials --resource-group=$AZURE_RG_NAME --name=$AZURE_SERVICE_NAME
